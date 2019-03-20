8f98c42ee7e703b3fd30232c4dc969b758c28652
simallaire <sp.allaire93@gmail.com>
2019-03-20 04:23:11 -0400

    auth usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function show(User $user){
     		return view('user.show',compact('user'));
 
