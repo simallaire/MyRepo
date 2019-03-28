1f239bd9e192f5f3c0db1c38f8165209a350195c
simallaire <sp.allaire93@gmail.com>
2019-03-28 07:57:25 -0400

    bug fix

--- resources/views/file/index.blade.php
+++ resources/views/file/index.blade.php
@@ -6,11 +6,13 @@
     <h4>Shared Files</h4>
     <div class="list-group">
         @foreach($files as $file)
-        <div class="list-group-item list-group-item-action">
-        <a href="/storage/files/{{$file->url}}" class="btn btn-default"><i class="far fa-file">&nbsp;&nbsp;</i>{{$file->originalName}}</a>
-
-        <p><i>by {{$file->post->user->name}}</i> from <a href="/post/{{$file->post->id}}">{{strip_tags($file->post->title)}}</a></p>
-        </div>
+            <div class="list-group-item list-group-item-action">
+            <a href="/storage/files/{{$file->url}}" class="btn btn-default"><i class="far fa-file">&nbsp;&nbsp;</i>{{$file->originalName}}</a>
+            @if(isset($file->post))
+                <p><i>by {{$file->post->user->name}}</i> 
+                from <a href="/post/{{$file->post->id}}">{{strip_tags($file->post->title)}}</a></p>
+            @endif
+            </div>
         @endforeach
     </div>
 
