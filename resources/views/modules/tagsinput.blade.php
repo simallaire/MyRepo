3b88bdfc15fa8ebfb3682aefe896019448f43b35
simallaire <sp.allaire93@gmail.com>
2019-04-26 04:21:25 -0400

    minor fix

--- resources/views/modules/tagsinput.blade.php
+++ resources/views/modules/tagsinput.blade.php
@@ -7,6 +7,6 @@
 		@endphp
 			<label class="subtitle is-5" for="tags">Tags	</label>
 			<input type="text" value="" name="tags" id="tag" class="input is-rounded" placeholder="Subject,Frameworks,Other languages, etc." value="" data-role="tagsinput" />
-			{{ implode(", #",$plucked->all()) }}
-			
+			{{ "#".implode(", #",$plucked->all()) }}
+	
 			
